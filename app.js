7cdd127c6a713e913f7830d2a8d5f8a5943a93bd
TuanWoox <z1venttt@gmail.com>
2024-07-25 21:31:27 +0700

    Done

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 //Routes
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
@@ -49,11 +51,15 @@ const sessionConfig = {
     resave: false,
     saveUninitialized: true,
     cookie: {
+        //name of the cookie!
+        name: 'blah',
         httpOnly: true,
         //Expires for old browser
         //expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         //MaxAge for new browser nowdays
         maxAge: 1000 * 60 * 60 * 24 * 7
+        //secure : only https can only work on this cookie
+        //secure:true
     }
 }
 app.use(session(sessionConfig));
@@ -73,7 +79,53 @@ app.use((req,res,next) => {
     res.locals.currentUser = req.user;
     next();
 })
-
+app.use(mongoSanitize());
+app.use(helmet());
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    'https://cdn.jsdelivr.net'
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    'https://cdn.jsdelivr.net'
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dztjmkvak/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 //Routers
 app.use('/',userRoutes);
 app.use('/campgrounds',campgroundRoutes);
