d3d21c5824070079588cb16eabb94ac52f55e3e9
TuanWoox <z1venttt@gmail.com>
2024-07-26 09:36:17 +0700

    Fixing URl

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 //     // and we can access it process.env.VARIABLENAME!
 // }
 //Npm packages
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -20,7 +21,7 @@ const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
-const dbUrl = DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 //Routes
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
